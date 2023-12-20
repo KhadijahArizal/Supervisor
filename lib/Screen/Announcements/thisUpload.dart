8624b373c32b7f75e6a640970860501be2598d07
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2023-12-20 22:05:40 +0800

    thisUpload.dart

--- lib/Screen/Announcements/thisUpload.dart
+++ lib/Screen/Announcements/thisUpload.dart
@@ -1,9 +1,14 @@
 class ThisUpload {
   dynamic title;
   dynamic info;
+  dynamic fileName;
+  dynamic date;
+
 
 
   ThisUpload(
       {required this.title,
-      required this.info});
+      required this.info,
+      required this.fileName,
+      required this.date});
 }
