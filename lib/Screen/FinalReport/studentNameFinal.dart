48dc9f4240d3d09cb79c407c1a1d15160b326952
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2023-12-20 22:03:01 +0800

    Add files via upload

---
+++ lib/Screen/FinalReport/studentNameFinal.dart
@@ -0,0 +1,44 @@
+final List<Map<String, dynamic>> allUsersF = [
+  {
+    "id": 1,
+    "name": 'Alice Johnson',
+    "matricNo": '2021001',
+    "status": 'approved',
+  },
+  {
+    "id": 2,
+    "name": "Bob Smith",
+    "matricNo": "2021002",
+    "status": 'approved',
+  },
+  {
+    "id": 3,
+    "name": "Charlie Brown",
+    "matricNo": "2021003",
+    "status": 'approved',
+  },
+  {
+    "id": 4,
+    "name": "David Williams",
+    "matricNo": "2021004",
+    "status": 'approved',
+  },
+  {
+    "id": 5,
+    "name": "Eva Davis",
+    "matricNo": "2021005",
+    "status": 'pending',
+  },
+  {
+    "id": 6,
+    "name": "Frank Miller",
+    "matricNo": "2021006",
+    "status": 'approved',
+  },
+  {
+    "id": 7,
+    "name": "Grace Taylor",
+    "matricNo": "2021007",
+    "status": 'approved',
+  },
+];
